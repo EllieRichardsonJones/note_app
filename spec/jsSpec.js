a66d63107677c92324cfa73638a9d42d53645053
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-03-04 15:12:36 +0000

    Refactoring code

--- spec/jsSpec.js
+++ spec/jsSpec.js
@@ -11,22 +11,17 @@ const matchers = (exp) => ({
   toEq: (val) => {
     if (exp === val) {
       console.log('%c    passed', 'color: #00ff00')
-      console.log('Passed')
     } else {
       console.log(`%c    Failed. (expect ${exp} to equal ${val})`, 'color: #ff0000')
-      console.log('Failed')
     }
   },
   isInstanceOf: (val) => {
     if (exp instanceof val) {
       console.log('%c    passed', 'color: #00ff00')
-      console.log('Passed')
     } else {
       console.log(`%c    Failed. (expect ${exp} to be instance of ${val})`, 'color: #ff0000')
-      console.log('Failed')
     }
   }
 })
 
 const expect = (exp) => matchers(exp)
-
--- spec/note_model_spec.js
+++ spec/note_model_spec.js
@@ -4,14 +4,3 @@ describe('Note', () => {
     expect(newNote.returnText()).toEq('this is a note')
   })
 })
-
-
-// var assert = {
-//   isTrue: function(assertionToCheck){
-//     if(!assertionToCheck){
-//       throw new Error("Assertion FAILED"+ assertionToCheck + "is not truthy")
-//     }
-//     else console.log("Test Passing")
-//   }
-// }
-// assert.isTrue(3>4)
