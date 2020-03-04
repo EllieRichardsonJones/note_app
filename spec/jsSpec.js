5f3b77f6b9288f50966d7a7812dbd4ce32299aaf
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-03-04 14:55:12 +0000

    Add output notes to html string - test passing

--- spec/jsSpec.js
+++ spec/jsSpec.js
@@ -1,25 +1,29 @@
 const describe = (description, fn) => {
+  console.log("Test: ")
   console.log(description)
   fn()
+  console.log("")
 }
 
-const it = (msg, fn) => {
-  console.log(' ' + msg, fn)
-}
+const it = (msg, fn) => describe(' ' + msg, fn)
 
 const matchers = (exp) => ({
   toEq: (val) => {
     if (exp === val) {
       console.log('%c    passed', 'color: #00ff00')
+      console.log('Passed')
     } else {
       console.log(`%c    Failed. (expect ${exp} to equal ${val})`, 'color: #ff0000')
+      console.log('Failed')
     }
   },
   isInstanceOf: (val) => {
     if (exp instanceof val) {
       console.log('%c    passed', 'color: #00ff00')
+      console.log('Passed')
     } else {
       console.log(`%c    Failed. (expect ${exp} to be instance of ${val})`, 'color: #ff0000')
+      console.log('Failed')
     }
   }
 })
--- spec/note_list_view_spec.js
+++ spec/note_list_view_spec.js
@@ -2,12 +2,12 @@ describe('NoteListView',() => {
   describe('.toHTML',() => {
     it('outputs Notes to html string', () => {
       var noteList = new NoteList()
+      var noteListView = new NoteListView(noteList)
       noteList.createNote('This is a note')
       noteList.createNote('This is a second note')
-      var NoteListView = new NoteListView(noteList)
 
       var expectedOutput = '<ul><li><div>This is a note</div></li><li><div>This is a second note</div></li></ul>'
-      expect(NoteListView.toHTML()).toEq(expectedOutput)
+      expect(noteListView.toHTML(noteList)).toEq(expectedOutput)
 
     })
   })
